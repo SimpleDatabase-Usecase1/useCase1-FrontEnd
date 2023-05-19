f663821324a86a0dde463cdf52af2013e2acb27b
Joycejlee00 <joycelee1294@gmail.com>
2023-05-18 20:39:32 -0400

    add routes for pages 1 - 3 and set default path to the Login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Auditor from './components/Auditor';
+import Account from './components/Account';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/auditor" element={<Auditor/>}/>
+      <Route path="/account" element={<Account/>}/>
+    </Routes>
   );
 }
 
