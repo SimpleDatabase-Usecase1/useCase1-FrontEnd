eaa8bc5e642be23ed14c87a26f2b2017b0b965ea
Joycejlee00 <joycelee1294@gmail.com>
2023-05-27 14:35:36 -0400

    add navbar components to different pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function Home() {
   return (
     <div>
+        {NavBar}
         <h1>This is the Home page</h1>
     </div>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Navbar, Nav} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const NavBar = () => {
   return (
-    <h1>navbar here</h1>
+    <Navbar bg="light" expand="lg">
+    {/* Page 1 - home */}
+      <LinkContainer to="/home">
+        <Navbar.Brand>Home Page</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+           {/* Page 2 - account management page */}
+          <LinkContainer to="/account">
+            <Nav.Link>Manage Account</Nav.Link>
+          </LinkContainer>
+           {/* Page 3 - auditor */}
+          <LinkContainer to="/auditor">
+            <Nav.Link>Auditor</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
