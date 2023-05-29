d771d96145dac597d055beb1a6fee4fe75370e32
Joycejlee00 <joycelee1294@gmail.com>
2023-05-29 16:38:30 -0400

    add table that is only visible to admin/manager

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 
 function Account() {
+
+  var getRole = sessionStorage.getItem("role");
+  const URL = 'http://localhost:8081/getAllAgents';
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUserInfo = async() => {
+      const { data } = await axios.get(URL);
+      setUsers(data);
+    };
+    fetchUserInfo();
+  }, []);
+
   return (
     <div>
       <NavBar/>
-      <h1>This is the Account Page</h1>
+      <br/>
+      <div className='account'>
+        <div className='container'>
+          
+          {/* Only root users (admin/manager) can see the table*/}
+          {getRole === 'root' ? 
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Keyword</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                <td> {user.username} </td>
+                <td> {user.password} </td>
+                <td> {user.keyword} </td>
+                <td> <button className='btn btn-warning'>Update</button> </td>
+                <td> <button className='btn btn-danger'>Delete</button> </td>
+              </tr>
+              ))}
+            </tbody>
+            <br/>
+            <div className="d-grid gap-2 col-50 mx-auto">
+              <button type='button' className='btn btn-outline-primary btn-lg'>Add User</button>
+            </div>
+          </table> :
+          <div>
+            <h2> This is the Account Modification page</h2>
+            <p>This page can only be modified by the manager/admin</p>
+          </div>
+          }
+          </div>
+      </div>
     </div>
+
   )
 }
 
