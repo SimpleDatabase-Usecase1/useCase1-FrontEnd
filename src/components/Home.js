7aa3831cf483886a9dd1e7363cbbe19fde5e0113
Joycejlee00 <joycelee1294@gmail.com>
2023-06-02 16:02:35 -0400

    add edit text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from './NavBar'
 
+
 function Home() {
+
   return (
     <div>
       <NavBar/>
-      <h4>This is the Home page</h4>
+
+      <h3>Welcome to the Home Page</h3>
+
+      <br/>
+      
       <p>This is a public page for all agents to visit</p>
+      
+      
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // https://getbootstrap.com/docs/4.0/getting-started/webpack/ <- ready to use css for navbar
 
